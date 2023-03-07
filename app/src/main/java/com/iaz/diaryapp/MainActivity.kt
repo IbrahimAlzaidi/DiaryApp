4ac72c168bede8256daca56cfac9b56bdb1c30d4
ibalzaidi <iaz@hippodata.net>
2023-03-07 21:24:30 +0100

    AuthenticationScreen completed.

--- app/src/main/java/com/iaz/diaryapp/MainActivity.kt
+++ app/src/main/java/com/iaz/diaryapp/MainActivity.kt
@@ -4,23 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.iaz.diaryapp.navigation.Screen
 import com.iaz.diaryapp.navigation.SetupNavGraph
 import com.iaz.diaryapp.ui.theme.DiaryAppTheme
+import com.iaz.diaryapp.util.Constants.APP_ID
+import io.realm.kotlin.mongodb.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window,false)
         setContent {
             DiaryAppTheme {
                 val navController = rememberNavController()
                 SetupNavGraph(
-                    startDestination = Screen.Authentication.route,
+                    startDestination = getStartDestination(),
                     navController = navController
                 )
             }
         }
     }
+}
+
+private fun getStartDestination(): String{
+    val user = App.create(APP_ID).currentUser
+    return if (user != null && user.loggedIn) Screen.Home.route else Screen.Authentication.route
 }
--- app/src/main/java/com/iaz/diaryapp/presentation/screens/AuthenticationScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.iaz.diaryapp.presentation.screens
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun AuthenticationScreen() {
-
-}
